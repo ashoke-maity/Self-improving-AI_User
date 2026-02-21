dae11e6b56a471e4d2ec4537bf9b37755889ae99
ashoke-maity <ashokemaity092@gmail.com>
2026-02-22 04:28:01 +0530

    first commit

--- server/configs/DB.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Successfully connected to Database !")
-};
-
-module.exports = connectDB;
--- server/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {AdminLogin, AdminRegister} = require('../controllers/adminController');
-
-router.post('/register', AdminRegister);
-router.post('/login', AdminLogin);
-
-module.exports = router;
